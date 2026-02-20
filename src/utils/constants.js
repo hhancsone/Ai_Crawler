0b0b1a7d9303c51e6a9f1823fe315fd6a1f5e253
hwd <703850016@qq.com>
2026-02-20 15:39:10 +0800

    Upload file: constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,71 @@
+export const newsSources = [
+  { 
+    id: 'wangyi', 
+    name: '网易新闻', 
+    baseUrl: '163.com', 
+    website: 'https://www.163.com',
+    categories: ['时事热点', '军事', '社会', '科技', '娱乐', '经济', '教育', '生活']
+  },
+  { 
+    id: 'souhu', 
+    name: '搜狐新闻', 
+    baseUrl: 'sohu.com', 
+    website: 'https://www.sohu.com',
+    categories: ['时政', '国际', '财经', '明星新闻', '综艺新闻', '影视音乐', '网红', '幼儿教育', '中小学', '高考', '高校', '考研考公', '教资法考', '留学', '学习资料', '时尚', '明星', '格调生活', '美容', '奢品', '男士', '生活方式', '通讯', '数码', '手机', '互联网', '5G', '智能硬件', '宇宙发现', '世界未解之谜', '科学发现', '物理帝国', '搜狐科学feed流', '经济解码', '股票', '基金', 'IPO', '新赛道', '搜狐酒业', '备孕指南', '怀胎十月', '生产必备', '月子', '新生儿', '历史', '国内资讯', '国际资讯', '风云人物', '战争历史', '军情纵横', '网红餐厅', '行业聚焦', '餐饮界', '休闲食品', '流行餐单', '读书', '人物', '收藏', '影视', '艺术', '运势', '情感', '性格解读', '生肖风水', '心理测试', '电竞', '手游', '单机', '网游', '攻略', '赛事追踪', '职业选手', '赛圈八卦', '搞笑feed流', '搞笑美女', '国漫推荐', '日漫推荐', '美漫推荐', '养宠经验', '喵星人', '汪星人']
+  },
+  { 
+    id: 'xinlang', 
+    name: '新浪新闻', 
+    baseUrl: 'sina.com.cn', 
+    website: 'https://news.sina.com.cn',
+    categories: ['国际', '国内', '社会', '科技', '体育', '娱乐', '财经', '教育', '军事']
+  },
+  { 
+    id: 'pengpai', 
+    name: '澎湃新闻', 
+    baseUrl: 'thepaper.cn', 
+    website: 'https://www.thepaper.cn',
+    categories: ['中国政库', '中南海', '舆论场', '打虎记', '人事风向', '法治中国', '一号专案', '港台来信', '长三角政商', '直击现场', '公益湃', '暖闻', '澎湃质量观', '绿政公署', '国防聚焦', '澎湃人物', '画外', '浦江头条', '上海大调研', '教育家', '全景现场', '美数课', '对齐Lab', '快看', '全球速报', '澎湃世界观', '澎湃明查', '澎湃防务', '外交学人', '唐人街', '大国外交', 'World全知道', '寰宇开放麦', '10%公司', '能见度', '地产界', '财经上下游', '区域经纬', '金改实验室', '牛市点线面', 'IPO最前线', '澎湃商学院', '自贸区连线', '新引擎', '进博会在线', '科学湃', '生命科学', '未来2%', '元宇宙观察', '科创101', '科学城邦', '澎湃研究所', '全球智库', '城市漫步', '市政厅', '世界会客厅', '社论', '澎湃评论', '思想湃', '上海书评', '思想市场', '私家历史', '上海文艺', '翻书党', '艺术评论', '古代艺术', '文化课', '逝者', '专栏', '异次元', '海平面', '一问三知', '有戏', '文艺范', '身体', '私·奔', '运动家', '非常品', '城势', '生活方式', '澎湃联播', '视界', '亲子学堂', '赢家', '汽车圈', 'IP SH', '酒业']
+  },
+  { 
+    id: 'ithome', 
+    name: 'IT之家', 
+    baseUrl: 'ithome.com', 
+    website: 'https://www.ithome.com',
+    categories: ['科技', '数码', '手机', '电脑', '软件', '游戏', '评测', '资讯']
+  },
+  { 
+    id: 'tengxunxinwen', 
+    name: '腾讯新闻', 
+    baseUrl: 'qq.com', 
+    website: 'https://news.qq.com',
+    categories: ['经济', '科技', '娱乐', '国际', '军事', '游戏', '民生', '房地产', '健康', '教育', '文化', '生活']
+  },
+  { 
+    id: 'tengxuntiyu', 
+    name: '腾讯体育', 
+    baseUrl: 'qq.com', 
+    website: 'https://sports.qq.com',
+    categories: ['NBA', 'CBA', '足球', '欧冠', '英超', '中超', '国际足球', '国内足球', '综合体育', '游泳', '田径']
+  },
+  { 
+    id: 'zhongguoribao', 
+    name: '中国日报', 
+    baseUrl: 'chinadaily.com.cn', 
+    website: 'https://www.chinadaily.com.cn',
+    categories: ['时政要闻', '台海动态', '台湾政策', '两岸人生', '国际资讯', '中国日报专稿', '传媒动态', '财经大事', '权威发布', '公告解读', '深度报道', '信息披露', '头条新闻', '旅游要闻', '酒店', '旅游原创', '业界资讯', '时尚', '健康频道', '教育', '体育']
+  },
+]
+
+export const simpleNewsSources = [
+  { id: 'wangyi', name: '网易新闻', baseUrl: '163.com', website: 'https://www.163.com' },
+  { id: 'souhu', name: '搜狐新闻', baseUrl: 'sohu.com', website: 'https://www.sohu.com' },
+  { id: 'xinlang', name: '新浪新闻', baseUrl: 'sina.com.cn', website: 'https://news.sina.com.cn' },
+  { id: 'tengxun', name: '腾讯新闻', baseUrl: 'qq.com', website: 'https://news.qq.com' },
+  { id: 'pengpai', name: '澎湃新闻', baseUrl: 'thepaper.cn', website: 'https://www.thepaper.cn' },
+  { id: 'ithome', name: 'IT之家', baseUrl: 'ithome.com', website: 'https://www.ithome.com' },
+  { id: 'huanqiu', name: '环球网', baseUrl: 'huanqiu.com', website: 'https://www.huanqiu.com' },
+  { id: 'zhongguoribao', name: '中国日报', baseUrl: 'chinadaily.com.cn', website: 'https://www.chinadaily.com.cn' },
+]
+
+export const defaultCategories = ['新闻', '科技', '体育', '国际新闻', '汽车', '游戏', '财经', '娱乐', '教育', '健康', '其他']
