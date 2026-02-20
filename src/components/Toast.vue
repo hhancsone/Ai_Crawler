64ed37d26f1e4c4b42e9fcc423ea28c066d65792
hwd <703850016@qq.com>
2026-02-20 15:39:08 +0800

    Upload file: Toast.vue

---
+++ src/components/Toast.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="['toast', show ? 'show' : '']">
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  message: String,
+  show: Boolean
+})
+
+defineEmits(['hide'])
+</script>
